ed46d58dd5f5488cfe131f90ed4030a98350dc32
Core <64542347+CoreDevelopment-UK@users.noreply.github.com>
2021-03-10 01:43:03 +0000

    misc changes

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import json
 import urllib.request
 import os
 import ctypes
-import random
 from datetime import datetime
 from astral.sun import sun
 from astral import LocationInfo
@@ -13,36 +12,9 @@ Settings
 SETTINGS = {
     "searhLimit": 5,
     "blacklist": [],
-    "subreddits-night": ['spaceporn', 'sunset'],
-    "subreddits-day": ['earthporn'],
-    "subreddits-time-based": True
+    "subreddit": 'earthporn',
+    "dark-background-at-night": True
 }
-
-''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Check if its Night
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ '''
-now = datetime.now() 
-now_time = now.time()
-city = LocationInfo("Dover", "England", "Europe/London", 51.154323, 1.290054)
-s = sun(city.observer, date=now)
-
-if (SETTINGS["subreddits-time-based"]): 
-    if now_time >= s["sunset"].time() or now_time <= s["sunrise"].time():
-        SETTINGS["subreddit"] = random.choice(SETTINGS["subreddits-night"])
-    else:
-        SETTINGS["subreddit"] = random.choice(SETTINGS["subreddits-day"])
-else:
-    SETTINGS["subreddit"] = random.choice(SETTINGS["subreddits-day"])
-
-''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Main
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ '''
-req = urllib.request.Request(f'https://www.reddit.com/r/{SETTINGS["subreddit"]}/top.json', headers = {'User-agent': 'Backgound Setter'})
-res = urllib.request.urlopen(req)
-j = json.load(res)
-SETTINGS['searhLimit'] = len(j['data']['children'])
-image = None
-current = 0
 
 ''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Image Filter
@@ -62,6 +34,13 @@ def Filter(x, y, url):
 ''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Find the image
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ '''
+req = urllib.request.Request(f'https://www.reddit.com/r/{SETTINGS["subreddit"]}/top.json', headers = {'User-agent': 'Backgound Setter'})
+res = urllib.request.urlopen(req)
+j = json.load(res)
+SETTINGS['searhLimit'] = len(j['data']['children'])
+current = 0
+image = None
+
 while not image:
     data = j['data']['children'][current]['data']
 
@@ -84,8 +63,26 @@ if not os.path.exists('images'):
 name = 'images\\' + image.split('/')[-1]
 urllib.request.urlretrieve(image, name)
 
-path = os.getcwd() + '\\' + name
+''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Check if its Night
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ '''
 SPI_SETDESKWALLPAPER = 20
 
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 0)
-print(f"The chosen image from r/{SETTINGS['subreddit']} is '{j['data']['children'][current]['data']['title']}' by u/{j['data']['children'][current]['data']['author']}.")
+if (SETTINGS["dark-background-at-night"]): 
+    now = datetime.now()
+    now_time = datetime.now().time()
+    city = LocationInfo("Dover", "England", "Europe/London", 51.154323, 1.290054)
+    s = sun(city.observer, date=now)
+
+    if now_time >= s["sunset"].time() or now_time <= s["sunrise"].time():
+        name = 'grey.png'
+        print(f"The chosen image was '{name}'")
+    else:
+        print(f"The chosen image was '{name}' | Subreddit: r/{SETTINGS['subreddit']} | Title: '{j['data']['children'][current]['data']['title']}' | User: u/{j['data']['children'][current]['data']['author']}.")
+
+    path = os.getcwd() + '\\' + name
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 0)
+else:
+    print(f"The chosen image was '{name}' | Subreddit: r/{SETTINGS['subreddit']} | Title: '{j['data']['children'][current]['data']['title']}' | User: u/{j['data']['children'][current]['data']['author']}.")
+    path = os.getcwd() + '\\' + name
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 0)

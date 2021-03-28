271ee8160e19c9a5a3401a787ae580218307965e
Paul <64542347+CoreDevelopment-UK@users.noreply.github.com>
2021-03-28 10:59:23 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,7 @@ SETTINGS = {
     "blacklist": [],
     "subreddits": ['earthporn'],
     "save-images": False,
-<<<<<<< HEAD
     "use-cache": False,
     "night-backgrounds": [],
-=======
-    "night-backgrounds": [],
->>>>>>> main
     "city": 'London'
 }

5eda4542745f6a4be7b1289c370d507072ca2335
Paul <64542347+coredev-uk@users.noreply.github.com>
2021-03-29 21:18:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ SETTINGS = {
     "subreddits": ['EarthPorn'],
     "save-images": False,
     "use-cache": False,
-    "night-backgrounds": ['https://i.imgur.com/0zhAp6x.png', 'https://i.imgur.com/Uyiy6ck.png', 'https://i.imgur.com/lJ6OeHg.jpg'],
+    "night-backgrounds": ['https://i.imgur.com/tIujN5Y.png', 'https://i.imgur.com/Uyiy6ck.png', 'https://i.imgur.com/lJ6OeHg.jpg'],
     "city": 'London'
 }

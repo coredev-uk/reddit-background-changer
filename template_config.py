a0c74b192b82751faa20f053b44fa794b05eb356
Core <64542347+coredev-uk@users.noreply.github.com>
2021-06-14 13:17:13 +0100

    updated template config

--- template_config.py
+++ template_config.py
@@ -3,24 +3,31 @@ import os
 SETTINGS = {
     "blacklist": [],
     "subreddits": ['EarthPorn'],
+    "notify": True,
     "diff-bg": False,
-    "monitor-x": "",
-    "monitor-y": "",
+
+    "res": {
+      "monitor-x": "",
+      "monitor-y": ""
+    },
+
     "night-backgrounds": {
         "toggle": True,
-        "diff-bg": False,
+        "diff-bg": True,
         "methods": {
-            "chosen-method": "All", # Link / Local / SubReddit / All
-            "links": [],
-            "subreddits": [],
-            "local": False,
+            "chosen-method": "Link", # Link / Local / SubReddit / All
+            "links": [""],
+            "subreddits": ["SpacePorn"],
+            "local": True,
         },
         "city": 'London',
         "notify": False
     },
+
     "downloaded-path": os.getcwd() + "\\img\\bg_Downloaded\\",
     "custom-path": os.getcwd() + "\\img\\bg_Custom\\",
     "active-path": os.getcwd() + "\\img\\bg_Active\\",
+
     "imgur": {
         "client_id": "",
         "client_secret": "",
@@ -29,6 +36,6 @@ SETTINGS = {
 }
 
 # dont touch this
-debug = False
+debug = True
 debugNotify = False
 debugNight = False
